a15b86603748943046be09830cbf1d559bfee05f
GuilhermeSerafim <guilerstudies@gmail.com>
2024-03-28 10:30:59 -0300

    Adicionar arquivos de projeto.

---
+++ ScreendSound/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+string mensagemDeBoasVindas = "Bem vindo ao Screend Sound\n";
+List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso" };
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+        ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+        ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+        ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+        ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+        ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+        ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
+}
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.Write(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoes()
+{
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 2:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case -1:
+            Console.WriteLine("Tchau tchau");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+
+}
+
+ExibirMensagemDeBoasVindas();
+ExibirLogo();
+ExibirOpcoes();
