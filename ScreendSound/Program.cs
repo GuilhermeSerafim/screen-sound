dcace886a38e490a928552e32b3110a6b1fadcc4
GuilhermeSerafim <guilerstudies@gmail.com>
2024-03-28 14:27:18 -0300

    Adiciona funcionalidade de escolha de banda com opções de visualizar, registrar e encerrar.

--- ScreendSound/Program.cs
+++ ScreendSound/Program.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 
 string mensagemDeBoasVindas = "Bem vindo ao Screend Sound\n";
-string[] listaDasBandas = ["0 - U2", "1 - The Beatles", "2 - Calypso", "3 - AC/DC", "4 - Guns N' Roses"];
+// Um Dictionary armazena pares de chave-valor, onde cada chave é única e associada a um valor.
+Dictionary<int, string> listaDasBandas = new Dictionary<int, string>()
+{
+    {0, "U2"},
+    {1, "The Beatles"},
+    {2, "Calypso"},
+    {3, "AC/DC"},
+    {4, "Guns N' Roses"}
+};
 
 
 void ExibirLogo()
@@ -21,49 +29,97 @@ void ExibirMensagemDeBoasVindas()
     Console.Write(mensagemDeBoasVindas);
 }
 
-int EscolhaABanda()
+int EscolhaABanda(int home)
 {
-    Console.WriteLine("Bandas: ");
-    for (int i = 0; i < listaDasBandas.Length; i++)
+    // Opção invalida
+    int opcaoEscolhidaNumerica = -4;
+    if (home == 1)
     {
-        Console.WriteLine(listaDasBandas[i]);
+        Console.WriteLine("Bandas: ");
+        foreach (var banda in listaDasBandas)
+        {
+            Console.WriteLine($"{banda.Key} - {banda.Value}");
+        }
+        Console.Write("\nDigite o número da sua banda: ");
+        string opcaoEscolhida = Console.ReadLine()!;
+        opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida); // Coloquei negativo por que aqui pode ser 2 ou 3
     }
-    Console.Write("\nDigite o número da sua banda: ");
-    string opcaoEscolhida = Console.ReadLine()!;
-    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
-  
+    else if (home == 2) // Escolha
+    {
+        opcaoEscolhidaNumerica = -2;
+    }
+    else if (home == 3) // Exit
+    {
+        opcaoEscolhidaNumerica = -3;
+    }
+
     switch (opcaoEscolhidaNumerica)
     {
-        case 0:
-            Console.WriteLine($"Você a banda {listaDasBandas[opcaoEscolhidaNumerica]}");
+        case -2:
+            Console.Clear();
+            Console.Write("Digite o nome da banda que deseja registrar: ");
+            string bandaRegistro = Console.ReadLine();
+            RegistrarBanda(bandaRegistro);
             break;
-        case 1:
-            Console.WriteLine($"Você a banda {listaDasBandas[opcaoEscolhidaNumerica]}");
-            break;
-        case 2:
-            Console.WriteLine($"Você a banda {listaDasBandas[opcaoEscolhidaNumerica]}");
-            break;
-        case 3:
-            Console.WriteLine($"Você a banda {listaDasBandas[opcaoEscolhidaNumerica]}");
-            break;
-        case 4:
-            Console.WriteLine($"Você a banda {listaDasBandas[opcaoEscolhidaNumerica]}");
-            break;
-        case -1:
-            Console.WriteLine("Tchau tchau");
+        case -3:
+            Console.WriteLine("Encerrando...");
             break;
         default:
-            Console.WriteLine("Opção inválida");
+            if (listaDasBandas.ContainsKey(opcaoEscolhidaNumerica))
+            {
+                Console.WriteLine($"Você escolheu a banda {listaDasBandas[opcaoEscolhidaNumerica]}");
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida");
+            }
             break;
     }
     return opcaoEscolhidaNumerica;
 }
 
-void IniciarPrograma()
+void RegistrarBanda(string bandaRegistro)
+{
+    // Adiciona a nova banda com o próximo número de identificação disponível
+    int novoIndice = listaDasBandas.Count;
+    listaDasBandas.Add(novoIndice, bandaRegistro);
+    Console.WriteLine("Banda adicionada!");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("Bandas disponíveis: ");
+    foreach (var banda in listaDasBandas)
+    {
+        Console.WriteLine($"{banda.Key} - {banda.Value}");
+    }
+    Thread.Sleep(1000);
+    int resultadoHome = Home(); // Para menu da home 
+    EscolhaABanda(resultadoHome); // Para escolher novamente 
+}
+
+int Home()
+{
+
+    Console.WriteLine("\nDigite 1 se para escolher um banda disponível");
+    Console.WriteLine("Digite 2 se deseja registrar uma banda");
+    Console.WriteLine("Digite 3 para encerrar");
+    string escolhaHome = Console.ReadLine();
+    int home = int.Parse(escolhaHome);
+
+    return home;
+}
+void AoIniciarOPrograma()
 {
     ExibirMensagemDeBoasVindas();
     ExibirLogo();
-    EscolhaABanda();
+    Console.WriteLine("\n\nBandas inicialmente disponiveis: ");
+    foreach (var banda in listaDasBandas)
+    {
+        Console.WriteLine($"{banda.Key} - {banda.Value}");
+    }
+    Console.WriteLine("\n\nCrie sua banda ou entre em uma!");
+    int registraBanda = Home(); // Armazena o resultado de Home() em uma variável
+    EscolhaABanda(registraBanda); // Chama EscolhaABanda() com o resultado de Home()
+
 }
 
-IniciarPrograma();
+AoIniciarOPrograma();
